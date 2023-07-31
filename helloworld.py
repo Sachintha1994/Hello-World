796cf332469dee9994748df8dfb22b2c469168f3
Sachintha1994 <sachinthathilina@gmail.com>
2023-07-31 15:02:16 +0530

    New commit 1

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+
+students_names = ["Samantha", 100, True, "Chamoda", "Ishini", "Rishi"]
+students_names[3:] = [200,300,400]
+students_names.append(300)
+students_names.insert(0,"Nimal")
+students_names.clear()
+print(students_names)
+print(len(students_names))
